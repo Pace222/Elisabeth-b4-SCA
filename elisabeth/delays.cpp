bf0697bfd617439f2a9228a112f7d0e71656f2e8
Pierugo Pace <pierugo.pace@nagra.com>
2024-07-03 11:44:30 +0200

    Corret key width, remember previous key, print delays

--- elisabeth/delays.cpp
+++ elisabeth/delays.cpp
@@ -1,7 +1,8 @@
 #include "delays.h"
+#include <stdio.h>
 #include <string.h>
 
-#include <SHA512.h>
+#include "SHA512.h"
 #define HASH_SIZE SHA512::HASH_SIZE / 2
 SHA512 H;
 
@@ -9,7 +10,7 @@ uint16_t delays[N_OPERATIONS];
 size_t head;
 uint16_t bias[N_OPERATIONS];
 uint16_t hash_chain[HASH_SIZE];
-uint4_t prev_key[KEYROUND_WIDTH_B4];
+uint4_t prev_key[KEY_WIDTH_B4];
 
 void hash(void* dest, void* src, size_t src_len) {
     H.reset();
@@ -18,17 +19,23 @@ void hash(void* dest, void* src, size_t src_len) {
 }
 
 void compute_delays(int compute_bias) {
+    if (!compute_bias)
+        printf("(");
     for (int i = 0; i < N_OPERATIONS; i += HASH_SIZE) {
         for (int j = 0; j < HASH_SIZE; ++j) {
-            if (i + j >= N_OPERATIONS) {
+            if (i + j >= N_OPERATIONS)
                 break;
-            }
-            if (compute_bias) {
+            if (compute_bias)
                 bias[i + j] = hash_chain[j] & 0b111111000;
-            }
             delays[i + j] = bias[i + j] | (hash_chain[j] & 0b111);
+            if (!compute_bias)
+                printf("%hX;", delays[i + j]);
         }
         hash(hash_chain, hash_chain, sizeof(hash_chain));
+    }
+    if (!compute_bias) {
+        printf(")");
+        fflush(stdout);
     }
 }
 
@@ -43,6 +50,7 @@ void init_chain(uint8_t* device_secret, size_t secret_len) {
 void new_encryption(uint4_t* new_key) {
     if (memcmp(prev_key, new_key, sizeof(prev_key))) {
         compute_delays(0);
+        memcpy(prev_key, new_key, sizeof(prev_key));
     }
 
     head = 0;
