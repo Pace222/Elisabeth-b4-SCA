f5baf6dda719d62b7a710f2e2d4302aac098b9ba
Pierugo Pace <pierugo.pace@nagra.com>
2024-07-09 13:30:32 +0200

    Less hashes necessary

--- elisabeth/delays.cpp
+++ elisabeth/delays.cpp
@@ -23,20 +23,23 @@ void hash(void* dest, void* src, size_t src_len) {
 }
 
 void compute_delays() {
-    for (int i = 0; i < N_OPERATIONS; i += HASH_SIZE) {
-        for (int j = 0; j < HASH_SIZE; ++j) {
-            if (i + j >= N_OPERATIONS)
-                break;
-
-            delays[i + j] = bias | (hash_chain_and_prev_key.hash_chain[j] & 0b111);
-        }
+    for (int i = 0; i < N_OPERATIONS; i += 2 * HASH_SIZE) {
         hash(&hash_chain_and_prev_key.hash_chain, &hash_chain_and_prev_key, sizeof(hash_chain_and_prev_key));
+        for (int j = 0; j < HASH_SIZE; ++j) {
+            if (i + 2*j >= N_OPERATIONS)
+                break;
+            delays[i + 2*j]     = bias | ((hash_chain_and_prev_key.hash_chain[j] & 0b11110000) >> 4);
+
+            if (i + 2*j + 1 >= N_OPERATIONS)
+                break;
+            delays[i + 2*j + 1] = bias | ((hash_chain_and_prev_key.hash_chain[j] & 0b00001111) >> 0);
+        }
     }
 }
 
 void init_chain(uint8_t* device_secret, size_t secret_len) {
     hash(&hash_chain_and_prev_key.hash_chain, device_secret, secret_len);
-    bias = hash_chain_and_prev_key.hash_chain[0] & 0b11111000;
+    bias = hash_chain_and_prev_key.hash_chain[0] & 0b11110000;
     memset(&hash_chain_and_prev_key.prev_key, 0, sizeof(hash_chain_and_prev_key.prev_key));
     compute_delays();
 
