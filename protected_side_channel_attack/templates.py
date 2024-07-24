44f6c25c70dc9c89fe643916a9bed91c319fd3d8
Pierugo Pace <pierugo.pace@nagra.com>
2024-07-24 14:10:24 +0200

    Redoing TVLA and template attacks with countermeasures

--- protected_side_channel_attack/templates.py
+++ protected_side_channel_attack/templates.py
@@ -38,51 +38,30 @@ class B4GaussianEstimator:
 def b4_train_test_split(*arrays, train_size: int, val_sizes: List[int]):
     return train_test_split(*arrays, train_size=train_size + max(val_sizes), random_state=0)
 
-def b4_gridsearch_cv(signal_strengths: List[SignalStrength], n_folds: int, train_sizes: List[int], val_sizes: List[int], num_features: List[int], traces_total: np.ndarray, labels_total: np.ndarray, masking: bool=False):
-    times = np.zeros((len(signal_strengths), len(train_sizes), len(val_sizes), len(num_features), n_folds), dtype=np.float64)
-    results = np.zeros((len(signal_strengths), len(train_sizes), len(val_sizes), len(num_features), n_folds, 2), dtype=np.float64)
-    
-    for s, sig_strength in enumerate(signal_strengths):
-        for ts, train_size in enumerate(train_sizes):
-            traces_trainval, traces_test, labels_trainval, labels_test = b4_train_test_split(traces_total, labels_total, train_size=train_size, val_sizes=val_sizes)
-            for m, val_size in enumerate(val_sizes):
-                rs = ShuffleSplit(n_splits=n_folds, test_size=val_size, train_size=train_size, random_state=0)
-                for cv, (train_index, val_index) in enumerate(rs.split(traces_trainval)):
-                    print(f"CV {cv}", end="\r")
-                    traces_train, traces_val = traces_trainval[train_index], traces_trainval[val_index]
-                    labels_train, labels_val = labels_trainval[train_index], labels_trainval[val_index]
-                    for n, n_feat in enumerate(num_features):
-                        if not masking:
-                            start = time()
-                            sig_strength.fit(traces_train, labels_train if not masking else labels_train[:, 1], n_feat) # Masking: use first share for feature selection
-                            features_train = sig_strength.transform(traces_train)
-                            gaussian_est = B4GaussianEstimator().fit(features_train, labels_train if not masking else 16 * labels_train[:, 0] + labels_train[:, 1])
-                            times[s, ts, m, n, cv] = time() - start
+def b4_gridsearch_cv(sig_strength: SignalStrength, n_folds: int, train_size: int, num_features: List[int], traces_trainval: np.ndarray, labels_trainval: np.ndarray):
+    rs = ShuffleSplit(n_splits=n_folds, train_size=train_size, random_state=0)
+    results = np.zeros((len(num_features), n_folds), dtype=np.float64)
+    models  = np.zeros((len(num_features), n_folds), dtype=object)
+    for cv, (train_index, val_index) in enumerate(rs.split(traces_trainval)):
+        print(f"CV {cv}", end="\r")
+        traces_train, traces_val = traces_trainval[train_index], traces_trainval[val_index]
+        labels_train, labels_val = labels_trainval[train_index], labels_trainval[val_index]
+        for n, n_feat in enumerate(num_features):
+            sig_strength.fit(traces_train, labels_train, n_feat)
+            features_train = sig_strength.transform(traces_train)
+            gaussian_est = B4GaussianEstimator().fit(features_train, labels_train)
 
-                            features_val = sig_strength.transform(traces_val)
-                            predicted = gaussian_est.predict(features_val)
-                            results[s, ts, m, n, cv] = np.count_nonzero(predicted == (labels_val if not masking else 16 * labels_val[:, 0] + labels_val[:, 1])) / labels_val.shape[0]
-                        else:
-                            start = time()
-                            sig_strength.fit(traces_train, labels_train[:, 1], n_feat) # Masking: use first share for feature selection
-                            features_train = sig_strength.transform(traces_train)
-                            gaussian_est_0 = B4GaussianEstimator().fit(features_train, labels_train[:, 0])
-                            gaussian_est_1 = B4GaussianEstimator().fit(features_train, labels_train[:, 1])
-                            times[s, ts, m, n, cv] = time() - start
+            features_val = sig_strength.transform(traces_val)
+            predicted = gaussian_est.predict(features_val)
+            results[n, cv] = np.count_nonzero(predicted == labels_val) / labels_val.shape[0]
+            models[n, cv] = gaussian_est
 
-                            features_val = sig_strength.transform(traces_val)
-                            predicted_0 = gaussian_est_0.predict(features_val)
-                            results[s, ts, m, n, cv, 0] = np.count_nonzero(predicted_0 == labels_val[:, 0]) / labels_val.shape[0]
-                            predicted_1 = gaussian_est_1.predict(features_val)
-                            results[s, ts, m, n, cv, 1] = np.count_nonzero(predicted_1 == labels_val[:, 1]) / labels_val.shape[0]
+        del traces_train, traces_val, features_train, features_val
+    for n, n_feat in enumerate(num_features):
+        print(f"{sig_strength} [num features: {n_feat}]: {np.mean(results[n]):#.4g} ± {np.std(results[n]):#.4g} ({results[n]}).")
+    print(f"-- BEST NUM_FEATURES: {num_features[np.argmax(np.mean(results, axis=1))]}--")
 
-                    del traces_train, traces_val, features_train, features_val
-                for n, n_feat in enumerate(num_features):
-                    print(f"{sig_strength} [num features: {n_feat}, train size: {train_size}, val size: {val_size}]: {np.mean(results[s, ts, m, n, :, 0]):#.4g} ± {np.std(results[s, ts, m, n, :, 0]):#.4g} ({results[s, ts, m, n, :, 0]}). Training in {np.mean(times[s, ts, m, n]):#.4g} ± {np.std(times[s, ts, m, n]):#.4g} seconds.")
-                    print(f"{sig_strength} [num features: {n_feat}, train size: {train_size}, val size: {val_size}]: {np.mean(results[s, ts, m, n, :, 1]):#.4g} ± {np.std(results[s, ts, m, n, :, 1]):#.4g} ({results[s, ts, m, n, :, 1]}). Training in {np.mean(times[s, ts, m, n]):#.4g} ± {np.std(times[s, ts, m, n]):#.4g} seconds.")
-            del traces_trainval, traces_test 
-                    
-    return times, results
+    return models[np.unravel_index(np.argmax(results), models.shape)], np.argmax(np.mean(results, axis=1)), np.max(np.mean(results, axis=1))
 
 def b4_train(sig_strength: SignalStrength, n_feat: int, traces_train: np.ndarray, labels_train: np.ndarray, masking: bool=False):
     sig_strength.fit(traces_train, labels_train if not masking else labels_train[:, 1], n_feat)
